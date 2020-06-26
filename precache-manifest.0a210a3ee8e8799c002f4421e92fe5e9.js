fb62fe41507eb22878b474be1b86c58b5313fc04
ansaamjad <ansaamjad555@gmail.com>
2020-06-26 18:40:31 +0500

    Updates

---
+++ precache-manifest.0a210a3ee8e8799c002f4421e92fe5e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06459e50e0b062c32b410e675f3e7b53",
+    "url": "/Todo-Application/index.html"
+  },
+  {
+    "revision": "9d50cea18a23437369f0",
+    "url": "/Todo-Application/static/css/main.90cf42d1.chunk.css"
+  },
+  {
+    "revision": "03fa6d297886ac488158",
+    "url": "/Todo-Application/static/js/2.0fa57b1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-Application/static/js/2.0fa57b1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d50cea18a23437369f0",
+    "url": "/Todo-Application/static/js/main.044aa1b1.chunk.js"
+  },
+  {
+    "revision": "67d22565bfda40171eb2",
+    "url": "/Todo-Application/static/js/runtime-main.328bf450.js"
+  }
+]);
